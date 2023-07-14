17089b1899248285fc68a186c8eba3ee2a4fdd03
alex <alexviejo@gmail.com>
2023-07-14 14:39:59 +0200

    full interactivo

--- ServerTCPInteractivo/src/aar/ServerTCPInteractivo.java
+++ ServerTCPInteractivo/src/aar/ServerTCPInteractivo.java
@@ -20,53 +20,54 @@ public class ServerTCPInteractivo {
             System.exit(1);
         }
 
-        Socket clientSocket = null;
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-
-        PrintWriter out = null;
-        BufferedReader in = null;
-        try {
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(
-				new InputStreamReader(
-				clientSocket.getInputStream()));
-        } catch (IOException e) {
-            System.err.println("Create streams failed.");
-            System.exit(1);
-        }
-            
-        ProtocoloComunicacion protocolo = new ProtocoloComunicacion();
-        
-        String inputLine, outputLine;
-        outputLine = protocolo.processInput(null);
-        out.println(outputLine);
-
-        try {
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("Recibo de cliente: "+inputLine);
-                outputLine = protocolo.processInput(inputLine);
-                out.println(outputLine);
-                if (outputLine.equals("Bye."))
-                    break;
+        while(true) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
             }
-        } catch (IOException e) {
-            System.err.println("Read failed.");
-            System.exit(1);
-        }
-             
-        try {
-            out.close();
-            in.close();
-            clientSocket.close();
-            serverSocket.close();  
-        } catch (IOException e) {
-            System.err.println("Close failed.");
-            System.exit(1);
+
+            PrintWriter out = null;
+            BufferedReader in = null;
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(
+                    new InputStreamReader(
+                    clientSocket.getInputStream()));
+            } catch (IOException e) {
+                System.err.println("Create streams failed.");
+                System.exit(1);
+            }
+                
+            ProtocoloComunicacion protocolo = new ProtocoloComunicacion();
+            
+            String inputLine, outputLine;
+            outputLine = protocolo.processInput(null);
+            out.println(outputLine);
+
+            try {
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("Recibo de cliente: "+inputLine);
+                    outputLine = protocolo.processInput(inputLine);
+                    out.println(outputLine);
+                    if (outputLine.equals("Bye."))
+                        break;
+                }
+            } catch (IOException e) {
+                System.err.println("Read failed.");
+                System.exit(1);
+            }
+                
+            try {
+                out.close();
+                in.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                System.err.println("Close failed.");
+                System.exit(1);
+            }
         }     
     } 
 }
